64af2459405fd0440d2ae3ad3484e5db5603669e
Bwemana Jonathan Marawes <82033719+Mabboi@users.noreply.github.com>
2025-03-15 14:06:05 +0100

    cors anoda fix

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const app = express();
 
 // Enable CORS for all origins
 app.use(cors({
-    origin: "http://localhost:3000", // Allows access from local website
-    methods: "GET,POST,PUT,DELETE",
+    origin: "http://192.168.18.29:3000", // Allows access from local website
     allowedHeaders: "Content-Type,Authorization",
     credentials: true
 }));
@@ -22,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Manually set CORS headers
 app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        res.header("Access-Control-Allow-Origin", "http://192.168.18.29:3000");
         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         res.header("Access-Control-Allow-Credentials", "true");
