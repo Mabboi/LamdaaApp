f1601c5a64a0821d28a10e36ceaec006926ccd6f
Bwemana Jonathan Marawes <82033719+Mabboi@users.noreply.github.com>
2025-03-15 18:49:43 +0100

    cors

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ connectDB();
 const app = express();
 
 // // Enable CORS for all origins
-// app.use(cors({
-//     origin: "*", // Allows access from local website
-//     allowedHeaders: "Content-Type,Authorization",
-//     credentials: true
-// }));
+app.use(cors({
+    origin: "*", // Change this to your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true // Allow cookies/session handling
+}));
 // // Middleware to parse JSON and URL-encoded data
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
@@ -30,14 +31,14 @@ const app = express();
 // app.use(cors());
 
 
-//Manually set CORS headers
-app.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.header("Access-Control-Allow-Credentials", "true");
-        next();
-    });
+// //Manually set CORS headers
+// app.use((req, res, next) => {
+//         res.header("Access-Control-Allow-Origin", "http://192.168.18.29:3000");
+//         res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//         res.header("Access-Control-Allow-Credentials", "true");
+//         next();
+//     });
 
 // Configure session
 app.use(session({
